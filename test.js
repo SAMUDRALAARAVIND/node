990a768f96972ede0b39195c253e2fd04bcc2900
SAMUDRALA ARAVIND <67514829+SAMUDRALAARAVIND@users.noreply.github.com>
2022-04-14 10:46:13 +0530

    Add files via upload

---
+++ test.js
@@ -0,0 +1,12 @@
+function CallMe() {
+
+  let obj = document.getElementById("test") ;
+  
+  obj.classList = ["mowa"] ;
+  console.log(obj.classList) ;
+}
+
+let arr = [1, 2, 3, 4]
+
+arr[0] = 178373 ;
+console.log(arr[0]) ;
